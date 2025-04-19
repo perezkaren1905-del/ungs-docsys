ac19260baca2136ad735e4403fb94d93b909126b
pabloabregu <pablo.abregu@hotmail.com>
2025-04-18 21:08:53 -0300

    Initial Docker Compose configuration

--- ungs-docsys-ms/src/main/java/com/ungs/docsys/mappers/DictamenDataMapper.java
+++ ungs-docsys-ms/src/main/java/com/ungs/docsys/mappers/DictamenDataMapper.java
@@ -7,12 +7,11 @@ import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface DictamenDataMapper {
     DictamenDataMapper INSTANCE = Mappers.getMapper(DictamenDataMapper.class);
 
     DictamenDataResponseDto toResponse(DictamenData modelExample);
 
     List<DictamenDataResponseDto> toResponses(List<DictamenData> dictamenDatas);
-
 }
